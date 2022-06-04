f496dde1f2e548d5dbabe256c4c9e55da05546ad
abdulhafeez <mrabdu965@gmail.com>
2022-06-04 01:03:28 -0700

    init commit

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,22 @@ const express = require("express")
 const app = express()
 const connectDB = require("./config/connectDB")
 connectDB()
+
+
 app.use(express.json())
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/products", require("./routes/productsRoutes"))
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")))
+
+  app.get("*", (req, res) => {
+    app.use(
+      express.static(path.resolve(__dirname, "client", "build", "index.html"))
+    )
+  })
+}
+
 app.listen(5000, () => console.log("Server is running"))
        
 
