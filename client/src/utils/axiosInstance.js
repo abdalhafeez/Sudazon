dfffeb538aa4f70837145d4d69d5eaf9275390d7
abdulhafeez <mrabdu965@gmail.com>
2022-06-24 22:15:57 -0700

    _

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 export const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://sudazon.herokuapp.com/api",
+  baseURL: "http://sudazon.herokuapp.com/api",
 });
 // export const PF = "http://localhost:8000"
 // export const PF = "https://sudanzon.herokuapp.com/"
--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.use("/api/cart", require("./routes/cartRoutes"))
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
   }
-const PORT = process.env.PORT || 500;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server is running"));
